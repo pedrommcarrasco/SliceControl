627548bde1350f8ea9c67664636d207a57258da4
Pedro Carrasco <pedrommcarrasco@gmail.com>
2019-01-17 19:32:20 +0000

    Initial commit

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+import SliceControl
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.navigationBar.isHidden = true
+        view.backgroundColor = .white
+    }
+}
