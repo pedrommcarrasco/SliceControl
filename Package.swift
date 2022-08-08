de2a54d25b85b13ae8c10730e4e58638803b0353
Pedro Carrasco <pedrommcarrasco>
2022-08-08 13:08:27 +0100

    Add SPM support

--- Package.swift
+++ Package.swift
@@ -1,20 +1,24 @@
 // swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
 let package = Package(
     name: "SliceControl",
     platforms: [
-       .iOS(.v11),
+       .iOS(.v11)
     ],
     products: [
         .library(
             name: "SliceControl",
-            targets: ["SliceControl"]),
+            targets: ["SliceControl"]
+        )
     ],
     dependencies: [],
     targets: [
         .target(
             name: "SliceControl",
             dependencies: [],
-            path: "SliceControl"),
+            path: "SliceControl"
+        )
     ]
 )
